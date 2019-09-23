b824ae7c334953b7db93d102ac6b6a40f9f18280
Sarah H <sarahrose.henderson@gmail.com>
2019-09-23 02:45:21 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,25 +1,36 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
-name_hash = {:blake => 500, :ashley => 2, :adam => 1}
+# # prereqs: iterators, hashes, conditional logic
+# # Given a hash with numeric values, return the key for the smallest value
+# name_hash = {:blake => 500, :ashley => 2, :adam => 1}
+# # def key_for_min_value(name_hash)
+# #     name_hash.empty?
+# #   name_hash.sort_by {|key, value| value}.first
+# #   end
+# #
+# #   key_for_min_value(name_hash)
+# 
 # def key_for_min_value(name_hash)
-#     name_hash.empty?
-#   name_hash.sort_by {|key, value| value}.first
+# # code goes here
+# min_value = nil
+# min_key = nil
+# name_hash.each do |k, v|
+#   if min_value == nil
+#     min_value = v
+#     min_key = k
+#   elsif min_value > v
+#     min_value = v
+#     min_key = k
 #   end
-#
-#   key_for_min_value(name_hash)
+# end
+# min_key
+# end
 
-def key_for_min_value(name_hash)
-# code goes here
-min_value = nil
-min_key = nil
-name_hash.each do |k, v|
-  if min_value == nil
-    min_value = v
-    min_key = k
-  elsif min_value > v
-    min_value = v
-    min_key = k
+ikea = {:chair => 25, :table => 85, :mattress => 450}
+
+def key_for_min(ikea)
+ikea.each do |item, price|
+  if price <= price
+return item
   end
-end
-min_key
-end
+  end
+  end
+  key_for_min(ikea)
